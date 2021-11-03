2183cf142a37edb6d55c8c0f29eb9b8c60941821
baiyonghong <879766788@qq.com>
2021-11-03 23:53:28 +0800

    commit eight week

--- eightWeek/main.js
+++ eightWeek/main.js
@@ -1,5 +1,36 @@
-import { ppp, test } from './test';
-let c = ppp(new Date().getTime());
-let p = 12125;
-var ddd = c + p;
-console.log(ddd);
+function createElement(type,attributes,...children){
+  let element;
+  if (typeof type === 'string'){
+    element = document.createElement(type);
+  } else {
+    element = new type;
+  }
+  for (let attrName in attributes){
+    element.setAttribute(attrName,attributes[attrName])
+  }
+  for (let child of children){
+    if (typeof child === 'string'){
+      child = document.createTextNode(child);
+    }
+    element.appendChild(child);
+  }
+  return element;
+}
+class Div{
+  constructor(){
+
+  }
+  setAttribute(){
+
+  }
+  appendChild(){
+
+  }
+  mountTo(parent){
+
+  }
+}
+let a = <Div id='a'>
+      <span>aweff<div>fff</div></span><span></span><span></span><span></span><span></span>
+</Div>
+a.mountTo(document.body)
