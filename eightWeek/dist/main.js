e803f07e56ce0ebfdbf806d2e43dbaec9c2aa797
baiyonghong <879766788@qq.com>
2021-11-03 23:52:48 +0800

    commit

--- eightWeek/dist/main.js
+++ eightWeek/dist/main.js
@@ -1,1 +1,32 @@
-(()=>{"use strict";var e=function(e){let t=function(e){return e+12345}((new Date).getTime());return t+"12578"}()+12125;console.log(e)})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./main.js":
+/*!*****************!*\
+  !*** ./main.js ***!
+  \*****************/
+/***/ (() => {
+
+eval("function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }\n\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }\n\nfunction createElement(type, attributes) {\n  var element;\n\n  if (typeof type === 'string') {\n    element = document.createElement(type);\n  } else {\n    element = new type();\n  }\n\n  for (var attrName in attributes) {\n    element.setAttribute(attrName, attributes[attrName]);\n  }\n\n  for (var _len = arguments.length, children = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {\n    children[_key - 2] = arguments[_key];\n  }\n\n  for (var _i = 0, _children = children; _i < _children.length; _i++) {\n    var child = _children[_i];\n\n    if (typeof child === 'string') {\n      child = document.createTextNode(child);\n    }\n\n    element.appendChild(child);\n  }\n\n  return element;\n}\n\nvar Div = /*#__PURE__*/function () {\n  function Div() {\n    _classCallCheck(this, Div);\n  }\n\n  _createClass(Div, [{\n    key: \"mountTo\",\n    value: function mountTo(parent) {}\n  }]);\n\n  return Div;\n}();\n\nvar a = createElement(Div, {\n  id: \"a\"\n}, createElement(\"span\", null, \"aweff\", createElement(\"div\", null, \"fff\")), createElement(\"span\", null), createElement(\"span\", null), createElement(\"span\", null), createElement(\"span\", null));\na.mountTo(document.body);\n\n//# sourceURL=webpack://jsx/./main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./main.js"]();
+/******/ 	
+/******/ })()
+;
