cfb2e683efca7f58574fb4f2acd2032e2e57fabd
b00284253 <baiyonghong@huawei.com>
2021-11-06 14:58:35 +0800

    [Author:] 木鱼 [Description:] 八周 轮播

--- eightWeek/dist/main.js
+++ eightWeek/dist/main.js
@@ -7,26 +7,90 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./frameWork.js":
+/*!**********************!*\
+  !*** ./frameWork.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"createElement\": () => (/* binding */ createElement),\n/* harmony export */   \"Component\": () => (/* binding */ Component)\n/* harmony export */ });\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }\n\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }\n\nfunction createElement(type, attributes) {\n  var element;\n\n  if (typeof type === 'string') {\n    element = new ElementWrapper(type);\n  } else {\n    element = new type();\n  }\n\n  for (var attrName in attributes) {\n    element.setAttribute(attrName, attributes[attrName]);\n  }\n\n  for (var _len = arguments.length, children = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {\n    children[_key - 2] = arguments[_key];\n  }\n\n  for (var _i = 0, _children = children; _i < _children.length; _i++) {\n    var child = _children[_i];\n\n    if (typeof child === 'string') {\n      child = new TextWrapper(child);\n    }\n\n    element.appendChild(child);\n  }\n\n  return element;\n}\nvar Component = /*#__PURE__*/function () {\n  function Component(type) {\n    _classCallCheck(this, Component);\n\n    this.root = document.createElement(type);\n  }\n\n  _createClass(Component, [{\n    key: \"setAttribute\",\n    value: function setAttribute(name, value) {\n      this.root.setAttribute(name, value);\n    }\n  }, {\n    key: \"appendChild\",\n    value: function appendChild(child) {\n      child.mountTo(this.root);\n    }\n  }, {\n    key: \"mountTo\",\n    value: function mountTo(parent) {\n      parent.appendChild(this.root);\n    }\n  }]);\n\n  return Component;\n}();\n\nvar ElementWrapper = /*#__PURE__*/function () {\n  function ElementWrapper(type) {\n    _classCallCheck(this, ElementWrapper);\n\n    this.root = document.createElement(type);\n  }\n\n  _createClass(ElementWrapper, [{\n    key: \"setAttribute\",\n    value: function setAttribute(name, value) {\n      this.root.setAttribute(name, value);\n    }\n  }, {\n    key: \"appendChild\",\n    value: function appendChild(child) {\n      child.mountTo(this.root);\n    }\n  }, {\n    key: \"mountTo\",\n    value: function mountTo(parent) {\n      parent.appendChild(this.root);\n    }\n  }]);\n\n  return ElementWrapper;\n}();\n\nvar TextWrapper = /*#__PURE__*/function () {\n  function TextWrapper(content) {\n    _classCallCheck(this, TextWrapper);\n\n    this.root = document.createTextNode(content);\n  }\n\n  _createClass(TextWrapper, [{\n    key: \"setAttribute\",\n    value: function setAttribute(name, value) {\n      this.root.setAttribute(name, value);\n    }\n  }, {\n    key: \"appendChild\",\n    value: function appendChild(child) {\n      child.mountTo(this.root);\n    }\n  }, {\n    key: \"mountTo\",\n    value: function mountTo(parent) {\n      parent.appendChild(this.root);\n    }\n  }]);\n\n  return TextWrapper;\n}();\n\n//# sourceURL=webpack://jsx/./frameWork.js?");
+
+/***/ }),
 
 /***/ "./main.js":
 /*!*****************!*\
   !*** ./main.js ***!
   \*****************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }\n\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }\n\nfunction createElement(type, attributes) {\n  var element;\n\n  if (typeof type === 'string') {\n    element = document.createElement(type);\n  } else {\n    element = new type();\n  }\n\n  for (var attrName in attributes) {\n    element.setAttribute(attrName, attributes[attrName]);\n  }\n\n  for (var _len = arguments.length, children = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {\n    children[_key - 2] = arguments[_key];\n  }\n\n  for (var _i = 0, _children = children; _i < _children.length; _i++) {\n    var child = _children[_i];\n\n    if (typeof child === 'string') {\n      child = document.createTextNode(child);\n    }\n\n    element.appendChild(child);\n  }\n\n  return element;\n}\n\nvar Div = /*#__PURE__*/function () {\n  function Div() {\n    _classCallCheck(this, Div);\n  }\n\n  _createClass(Div, [{\n    key: \"mountTo\",\n    value: function mountTo(parent) {}\n  }]);\n\n  return Div;\n}();\n\nvar a = createElement(Div, {\n  id: \"a\"\n}, createElement(\"span\", null, \"aweff\", createElement(\"div\", null, \"fff\")), createElement(\"span\", null), createElement(\"span\", null), createElement(\"span\", null), createElement(\"span\", null));\na.mountTo(document.body);\n\n//# sourceURL=webpack://jsx/./main.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _frameWork__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./frameWork */ \"./frameWork.js\");\nfunction _typeof(obj) { \"@babel/helpers - typeof\"; if (typeof Symbol === \"function\" && typeof Symbol.iterator === \"symbol\") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === \"function\" && obj.constructor === Symbol && obj !== Symbol.prototype ? \"symbol\" : typeof obj; }; } return _typeof(obj); }\n\nfunction _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== \"undefined\" && o[Symbol.iterator] || o[\"@@iterator\"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === \"number\") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError(\"Invalid attempt to iterate non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.\"); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it[\"return\"] != null) it[\"return\"](); } finally { if (didErr) throw err; } } }; }\n\nfunction _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === \"string\") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === \"Object\" && o.constructor) n = o.constructor.name; if (n === \"Map\" || n === \"Set\") return Array.from(o); if (n === \"Arguments\" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }\n\nfunction _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }\n\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }\n\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }\n\nfunction _inherits(subClass, superClass) { if (typeof superClass !== \"function\" && superClass !== null) { throw new TypeError(\"Super expression must either be null or a function\"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }\n\nfunction _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }\n\nfunction _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }\n\nfunction _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === \"object\" || typeof call === \"function\")) { return call; } else if (call !== void 0) { throw new TypeError(\"Derived constructors may only return object or undefined\"); } return _assertThisInitialized(self); }\n\nfunction _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError(\"this hasn't been initialised - super() hasn't been called\"); } return self; }\n\nfunction _isNativeReflectConstruct() { if (typeof Reflect === \"undefined\" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === \"function\") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }\n\nfunction _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }\n\n\n\nvar Carousel = /*#__PURE__*/function (_Component) {\n  _inherits(Carousel, _Component);\n\n  var _super = _createSuper(Carousel);\n\n  function Carousel() {\n    var _this;\n\n    _classCallCheck(this, Carousel);\n\n    _this = _super.call(this);\n    _this.attributes = new Object();\n    return _this;\n  }\n\n  _createClass(Carousel, [{\n    key: \"setAttribute\",\n    value: function setAttribute(name, value) {\n      this.attributes[name] = value;\n    } // 手动拖拽支持\n\n  }, {\n    key: \"setManualDrag\",\n    value: function setManualDrag() {\n      var childrens = this.root.children;\n      var position = 0;\n      this.root.addEventListener('mousedown', function (event) {\n        var startX = event.clientX;\n\n        var move = function move(event) {\n          var x = event.clientX - startX;\n          var current = position - (x - x % 500) / 500;\n\n          for (var _i = 0, _arr = [-1, 0, 1]; _i < _arr.length; _i++) {\n            var offset = _arr[_i];\n            var pos = current + offset;\n            pos = (pos + childrens.length) % childrens.length;\n            childrens[pos].style.transition = 'none';\n            childrens[pos].style.transform = \"translateX(\".concat(-pos * 500 + offset * 500 + x % 500, \"px)\");\n          }\n        };\n\n        var up = function up(event) {\n          var x = event.clientX - startX;\n          position = position - Math.round(x / 500);\n\n          for (var _i2 = 0, _arr2 = [0, -Math.sign(Math.round(x / 500) - x + 250 * Math.sign(x))]; _i2 < _arr2.length; _i2++) {\n            var offset = _arr2[_i2];\n            var pos = position + offset;\n            pos = (pos + childrens.length) % childrens.length;\n            childrens[pos].style.transition = '';\n            childrens[pos].style.transform = \"translateX(\".concat(-pos * 500 + offset * 500, \"px)\");\n          }\n\n          document.removeEventListener('mousemove', move);\n          document.removeEventListener('mouseup', up);\n        };\n\n        document.addEventListener('mousemove', move);\n        document.addEventListener('mouseup', up);\n      });\n    } // 自动轮播\n\n  }, {\n    key: \"setAutoRound\",\n    value: function setAutoRound() {\n      var childrens = this.root.children;\n      var currentIndex = 0;\n      setInterval(function () {\n        var nextIndex = (currentIndex + 1) % childrens.length;\n        var current = childrens[currentIndex];\n        var next = childrens[nextIndex];\n        next.style.transition = 'none';\n        next.style.transform = \"translateX(\".concat(100 - 100 * nextIndex, \"%)\");\n        setTimeout(function () {\n          next.style.transition = '';\n          current.style.transform = \"translateX(\".concat(-nextIndex * 100, \"%)\");\n          next.hidden = false;\n          next.style.transform = \"translateX(\".concat(-nextIndex * 100, \"%)\");\n          currentIndex = nextIndex;\n        });\n      }, 3000);\n    }\n  }, {\n    key: \"render\",\n    value: function render() {\n      this.root = document.createElement('div');\n      this.root.classList.add('carousel');\n\n      var _iterator = _createForOfIteratorHelper(this.attributes.src || []),\n          _step;\n\n      try {\n        for (_iterator.s(); !(_step = _iterator.n()).done;) {\n          var img = _step.value;\n          var child = document.createElement('img');\n          child.src = img;\n          child.setAttribute('draggable', false);\n          this.root.appendChild(child);\n        } // this.setAutoRound();\n\n      } catch (err) {\n        _iterator.e(err);\n      } finally {\n        _iterator.f();\n      }\n\n      this.setManualDrag();\n      return this.root;\n    }\n  }, {\n    key: \"mountTo\",\n    value: function mountTo(parent) {\n      parent.appendChild(this.render());\n    }\n  }]);\n\n  return Carousel;\n}(_frameWork__WEBPACK_IMPORTED_MODULE_0__.Component);\n\nvar imgList = ['https://developer.huawei.com/images/home/bbbs.jpg', 'https://developer.huawei.com/images/new-content/home/new-1-x.jpg', 'https://developer.huawei.com/images/new-content/home/zixun-4.jpg', 'https://developer.huawei.com/images/new-content/home/new-maoxiaozhi.jpg', 'https://developer.huawei.com/images/new-content/home/new-ar.jpg'];\nvar carousel = createElement(Carousel, {\n  src: imgList\n});\nconsole.log(123);\ncarousel.mountTo(document.body);\n\n//# sourceURL=webpack://jsx/./main.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./main.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./main.js");
 /******/ 	
 /******/ })()
 ;
--- eightWeek/main.js
+++ eightWeek/main.js
@@ -1,36 +1,88 @@
-function createElement(type,attributes,...children){
-  let element;
-  if (typeof type === 'string'){
-    element = document.createElement(type);
-  } else {
-    element = new type;
+import { Component } from './frameWork';
+class Carousel extends Component {
+  constructor() {
+    super();
+    this.attributes = new Object();
   }
-  for (let attrName in attributes){
-    element.setAttribute(attrName,attributes[attrName])
+  setAttribute(name, value) {
+    this.attributes[name] = value;
   }
-  for (let child of children){
-    if (typeof child === 'string'){
-      child = document.createTextNode(child);
+
+  // 手动拖拽支持
+  setManualDrag() {
+    let childrens = this.root.children;
+    let position = 0;
+    this.root.addEventListener('mousedown', (event) => {
+      let startX = event.clientX;
+      let move = (event) => {
+        let x = event.clientX - startX;
+        let current = position - (x - (x % 500)) / 500;
+        for (let offset of [-1, 0, 1]) {
+          let pos = current + offset;
+          pos = (pos + childrens.length) % childrens.length;
+          childrens[pos].style.transition = 'none';
+          childrens[pos].style.transform = `translateX(${-pos * 500 + offset * 500 + (x % 500)}px)`;
+        }
+      };
+      let up = (event) => {
+        let x = event.clientX - startX;
+        position = position - Math.round(x / 500);
+        for (let offset of [0, -Math.sign(Math.round(x / 500) - x + 250 * Math.sign(x))]) {
+          let pos = position + offset;
+          pos = (pos + childrens.length) % childrens.length;
+          childrens[pos].style.transition = '';
+          childrens[pos].style.transform = `translateX(${-pos * 500 + offset * 500}px)`;
+        }
+        document.removeEventListener('mousemove', move);
+        document.removeEventListener('mouseup', up);
+      };
+      document.addEventListener('mousemove', move);
+      document.addEventListener('mouseup', up);
+    });
+  }
+  // 自动轮播
+  setAutoRound() {
+    let childrens = this.root.children;
+    let currentIndex = 0;
+    setInterval(() => {
+      let nextIndex = (currentIndex + 1) % childrens.length;
+      let current = childrens[currentIndex];
+      let next = childrens[nextIndex];
+      next.style.transition = 'none';
+      next.style.transform = `translateX(${100 - 100 * nextIndex}%)`;
+      setTimeout(() => {
+        next.style.transition = '';
+        current.style.transform = `translateX(${-nextIndex * 100}%)`;
+        next.hidden = false;
+        next.style.transform = `translateX(${-nextIndex * 100}%)`;
+        currentIndex = nextIndex;
+      });
+    }, 3000);
+  }
+  render() {
+    this.root = document.createElement('div');
+    this.root.classList.add('carousel');
+    for (let img of this.attributes.src || []) {
+      let child = document.createElement('img');
+      child.src = img;
+      child.setAttribute('draggable', false);
+      this.root.appendChild(child);
     }
-    element.appendChild(child);
+    // this.setAutoRound();
+    this.setManualDrag();
+    return this.root;
   }
-  return element;
-}
-class Div{
-  constructor(){
-
-  }
-  setAttribute(){
-
-  }
-  appendChild(){
-
-  }
-  mountTo(parent){
-
+  mountTo(parent) {
+    parent.appendChild(this.render());
   }
 }
-let a = <Div id='a'>
-      <span>aweff<div>fff</div></span><span></span><span></span><span></span><span></span>
-</Div>
-a.mountTo(document.body)
+let imgList = [
+  'https://developer.huawei.com/images/home/bbbs.jpg',
+  'https://developer.huawei.com/images/new-content/home/new-1-x.jpg',
+  'https://developer.huawei.com/images/new-content/home/zixun-4.jpg',
+  'https://developer.huawei.com/images/new-content/home/new-maoxiaozhi.jpg',
+  'https://developer.huawei.com/images/new-content/home/new-ar.jpg',
+];
+let carousel = <Carousel src={imgList} />;
+console.log(123);
+carousel.mountTo(document.body);
